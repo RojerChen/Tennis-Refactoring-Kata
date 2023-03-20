b72f042fd83c2a51578eba29943e081a5003ffc2
RojerChen <rojerchen@hotmail.com>
2023-03-20 22:50:02 +0800

    inline first GetScore

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -60,7 +60,24 @@ namespace Tennis
             else
             {
                 //normal score
-                string score2 = GetScore(player1Score, "");
+                string score = "";
+                switch (player1Score)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+
+                string score2 = score;
                 score2 += "-";
                 return GetScore(player2Score, score2);
             }
